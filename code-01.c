6ee334f7e103978c098299e8f41b1504b0cb2d39
Pattarakarn <pattarakarn.b@ku.th>
2024-01-22 14:01:06 +0700

    Update Hello Git in C and Hello Git in C++

--- code-01.c
+++ code-01.c
@@ -2,7 +2,7 @@
 
 int main()
 {
-
-    printf("Hello Git\n");
+    char text[] = "Hello Git";
+    printf("%s\n", text);
     
 }
---
+++ code-02.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // c out : console output
+    // endl : end line '\n'
+    cout << "Hello Git" << endl;
+
+}
